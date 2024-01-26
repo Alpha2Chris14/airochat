9db488f94f5683bec4a0ef3cced5511511efa2d1
Alpha2Chris14 <onyekachristian14@gmail.com>
2024-01-26 19:49:19 +0100

    made several modification

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
+import 'package:airochat/pages/auth_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'package:airochat/services/auth/login_or_register.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginOrRegister(),
+      home: AuthPage(),
     );
   }
 }
---
+++ lib/pages/auth_page.dart
@@ -0,0 +1,20 @@
+import 'package:airochat/pages/home_page.dart';
+import 'package:airochat/pages/login_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class AuthPage extends StatelessWidget {
+  const AuthPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return HomePage();
+          }
+          return LoginPage();
+        });
+  }
+}
