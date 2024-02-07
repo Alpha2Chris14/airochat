f9b4be3cf59473797a308b4a9fd7d96e16c84795
Alpha2Chris14 <onyekachristian14@gmail.com>
2024-02-07 06:44:51 +0100

    chat  room serveice

--- lib/services/chat/chat_service.dart
+++ lib/services/chat/chat_service.dart
@@ -1,3 +1,4 @@
+import 'package:airochat/model/message.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -10,13 +11,43 @@ class ChatService extends ChangeNotifier {
   // Send Message
   Future<void> sendMessage(String recieverId, String message) async {
     //get current user info
-
+    final String currentUserId = _firebaseAuth.currentUser!.uid;
+    final String currentUserEmaail =
+        _firebaseAuth.currentUser!.email.toString();
+    final Timestamp timestamp = Timestamp.now();
     //create a new message
+    Message newMessage = Message(
+      senderId: currentUserId,
+      senderEmail: currentUserEmaail,
+      recieverId: recieverId,
+      message: message,
+      timestamp: timestamp,
+    );
 
     //construct chat room id from current user id and reciever id(sorted to ensure uniqueness)
-
+    List<String> ids = [currentUserId, recieverId];
+    ids.sort(); //sort the ids (this ensures the chat room id is always the same for any pair of data)
+    String chatRoomId = ids
+        .join("_"); //combine the ids into a single string to use as chatroom id
     //add new message to database
+    _firestore
+        .collection('chat_rooms')
+        .doc(chatRoomId)
+        .collection('messages')
+        .add(newMessage.toMap());
   }
 
   //GET MESSAGES
+  Stream<QuerySnapshot> getMessages(String userId, String otherUserId) {
+    // construct chat room id from user ids (sorted to ensure it matches the id used when ending message)
+    List<String> ids = [userId, otherUserId];
+    ids.sort();
+    String chatRoomId = ids.join("_");
+    return _firestore
+        .collection('chat_rom')
+        .doc(chatRoomId)
+        .collection('messages')
+        .orderBy('timestamp', descending: false)
+        .snapshots();
+  }
 }
